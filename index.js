bc926da8a124ebb12369028c58643b8573344531
Tran Van <vanbaotran201194@gmail.com>
2021-04-15 21:28:00 +0200

    all done except for bonus

--- index.js
+++ index.js
@@ -3,17 +3,52 @@ class SortedList {
     this.items=[];
     this.length=this.items.length;
   }
-  add(item) {}
+  add(item) { 
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function (a,b){
+      return a-b;
+    })  
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos<=this.length-1){
+    return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length !== 0){
+      return this.items[this.length-1];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+  min() {
+    if (this.length !== 0){
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  sum() {
+    if (this.length===0){
+      return 0;
+    } else {
+      this.items.reduce((acc,currentVal)=>acc+currentVal,0)
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length===0){
+      return 0;
+    } else {
+      this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
